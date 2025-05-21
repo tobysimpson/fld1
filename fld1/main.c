bdb12eba7dafd874574f31f64afbdad544ff05c3
Toby Simpson <tobygeneva@hotmail.com>
2025-05-21 20:03:21 +0200

    Initial Commit

---
+++ fld1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  fld1
+//
+//  Created by Toby Simpson on 21.05.2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
